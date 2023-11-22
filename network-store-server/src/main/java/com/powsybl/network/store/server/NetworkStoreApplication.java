5bd2232ed0da23daeb0bc975390272f101844ccb
sBouzols <sylvain.bouzols@gmail.com>
2023-11-22 20:51:05 +0100

    update to powsybl-ws-dependencies 2.6.0
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- network-store-server/src/main/java/com/powsybl/network/store/server/NetworkStoreApplication.java
+++ network-store-server/src/main/java/com/powsybl/network/store/server/NetworkStoreApplication.java
@@ -2,7 +2,6 @@ package com.powsybl.network.store.server;
 
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
-import com.powsybl.ws.commons.Utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 public class NetworkStoreApplication {
 
     public static void main(String[] args) {
-        Utils.initProperties();
         SpringApplication.run(NetworkStoreApplication.class, args);
     }
 
