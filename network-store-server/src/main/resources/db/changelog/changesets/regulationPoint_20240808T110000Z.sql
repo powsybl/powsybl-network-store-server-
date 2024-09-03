a0f276e5f74c263fad7918e83b603a1d5b4338e1
Etienne LESOT <etienne.lesot@rte-france.com>
2024-09-03 15:30:28 +0200

    fix
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- network-store-server/src/main/resources/db/changelog/changesets/regulationPoint_20240808T110000Z.sql
+++ network-store-server/src/main/resources/db/changelog/changesets/regulationPoint_20240808T110000Z.sql
@@ -8,5 +8,5 @@ insert into regulationpoint (networkuuid, variantnum, regulatedequipmentid, equi
 select networkuuid, variantnum, id, 'SHUNT_COMPENSATOR', null, id, null, regulatingterminal, null
 from shuntcompensator;
 insert into regulationpoint (networkuuid, variantnum, regulatedequipmentid, equipmenttype, regulationmode, localterminalconnectableid, localterminalside, regulatingterminalconnectableid, regulatingterminalside)
-select networkuuid, variantnum, id, 'HVDC_CONVERTER_STATION', null, id, null, regulatingterminal, null
+select networkuuid, variantnum, id, 'VSC_CONVERTER_STATION', null, id, null, regulatingterminal, null
 from vscconverterstation;
