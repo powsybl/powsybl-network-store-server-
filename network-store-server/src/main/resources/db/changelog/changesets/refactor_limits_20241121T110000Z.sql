e434daa84e284383bc02c604339146df1ed476cb
Etienne LESOT <etienne.lesot@rte-france.com>
2024-12-06 15:30:25 +0100

    change java migration to sql migration
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

---
+++ network-store-server/src/main/resources/db/changelog/changesets/refactor_limits_20241121T110000Z.sql
@@ -0,0 +1,21 @@
+INSERT INTO newtemporarylimits (equipmentid, equipmenttype, networkuuid, variantnum, temporarylimits)
+SELECT equipmentid, equipmenttype, networkuuid, variantnum,
+       json_agg(json_build_object(
+               'operationalLimitsGroupId', operationalLimitsGroupId,
+               'side', side,
+               'limitType', limitType,
+               'name', name,
+               'value', value_,
+               'acceptableDuration', acceptableduration,
+               'fictitious', fictitious
+                )) as temporarylimits FROM temporarylimit
+GROUP  BY equipmentid, equipmenttype, networkuuid, variantnum;
+INSERT INTO newpermanentlimits (equipmentid, equipmenttype, networkuuid, variantnum, permanentlimits)
+SELECT equipmentid, equipmenttype, networkuuid, variantnum,
+       json_agg(json_build_object(
+               'operationalLimitsGroupId', operationalLimitsGroupId,
+               'value', value_,
+               'side', side,
+               'limitType', limitType
+                )) as permanentlimits FROM permanentlimit
+GROUP  BY equipmentid, equipmenttype, networkuuid, variantnum;
