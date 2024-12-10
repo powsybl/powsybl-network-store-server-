548cd0e6702ffa31d621c0486dd4b9c1f1259147
Etienne LESOT <etienne.lesot@rte-france.com>
2024-12-10 10:26:58 +0100

    fix changelog
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- network-store-server/src/main/resources/db/changelog/changesets/move_regulating_to_regulationPoint_20241204T110000Z.sql
+++ network-store-server/src/main/resources/db/changelog/changesets/move_regulating_to_regulationPoint_20241204T110000Z.sql
@@ -1,29 +1,29 @@
 -- generator
 update regulatingpoint
 set regulating = generator.voltageregulatoron from generator
-where regulatingequipmenttype = 'GENERATOR'
-  and regulatingequipmentid = generator.id
+where regulatingpoint.regulatingequipmenttype = 'GENERATOR'
+  and regulatingpoint.regulatingequipmentid = generator.id
   and regulatingpoint.networkuuid = generator.networkuuid
   and regulatingpoint.variantnum = generator.variantnum;
 -- vsc
 update regulatingpoint
 set regulating = vscconverterstation.voltageregulatoron
 from vscconverterstation
-where regulatingequipmenttype = 'VSC_CONVERTER_STATION'
-  and regulatingequipmentid = vscconverterstation.id
+where regulatingpoint.regulatingequipmenttype = 'VSC_CONVERTER_STATION'
+  and regulatingpoint.regulatingequipmentid = vscconverterstation.id
   and regulatingpoint.networkuuid = vscconverterstation.networkuuid
   and regulatingpoint.variantnum = vscconverterstation.variantnum;
 -- shunt
 update regulatingpoint
 set regulating = shuntcompensator.voltageregulatoron
 from shuntcompensator
-where regulatingequipmenttype = 'SHUNT_COMPENSATOR'
-  and regulatingequipmentid = shuntcompensator.id
+where regulatingpoint.regulatingequipmenttype = 'SHUNT_COMPENSATOR'
+  and regulatingpoint.regulatingequipmentid = shuntcompensator.id
   and regulatingpoint.networkuuid = shuntcompensator.networkuuid
   and regulatingpoint.variantnum = shuntcompensator.variantnum;
 
 -- svc
 update regulatingpoint
-set regulating = (CASE WHEN regulatingpoint.regulationmode = 'VOLTAGE'
+set regulating = (CASE WHEN regulationmode = 'VOLTAGE'
                            THEN TRUE ELSE FALSE END)
-where regulatingpoint.regulatingequipmenttype = 'STATIC_VAR_COMPENSATOR';
+where regulatingequipmenttype = 'STATIC_VAR_COMPENSATOR';
