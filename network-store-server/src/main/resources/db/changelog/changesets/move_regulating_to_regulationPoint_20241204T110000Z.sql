47ee001fa3258cfd6e4e8a1947b784d3a3159b87
Etienne LESOT <etienne.lesot@rte-france.com>
2024-12-10 13:22:57 +0100

    fix sql migration
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- network-store-server/src/main/resources/db/changelog/changesets/move_regulating_to_regulationPoint_20241204T110000Z.sql
+++ network-store-server/src/main/resources/db/changelog/changesets/move_regulating_to_regulationPoint_20241204T110000Z.sql
@@ -1,27 +1,28 @@
 -- generator
 update regulatingpoint
-set regulating = generator.voltageregulatoron from generator
-where regulatingpoint.regulatingequipmenttype = 'GENERATOR'
-  and regulatingpoint.regulatingequipmentid = generator.id
-  and regulatingpoint.networkuuid = generator.networkuuid
-  and regulatingpoint.variantnum = generator.variantnum;
+set regulating = (select generator.voltageregulatoron from generator
+            where regulatingequipmenttype = 'GENERATOR'
+              and regulatingequipmentid = generator.id
+              and regulatingpoint.networkuuid = generator.networkuuid
+              and regulatingpoint.variantnum = generator.variantnum)
+where regulatingequipmenttype = 'GENERATOR';
 -- vsc
+
 update regulatingpoint
-set regulating = vscconverterstation.voltageregulatoron
-from vscconverterstation
-where regulatingpoint.regulatingequipmenttype = 'VSC_CONVERTER_STATION'
-  and regulatingpoint.regulatingequipmentid = vscconverterstation.id
-  and regulatingpoint.networkuuid = vscconverterstation.networkuuid
-  and regulatingpoint.variantnum = vscconverterstation.variantnum;
+set regulating = (select vscconverterstation.voltageregulatoron from vscconverterstation
+            where regulatingpoint.regulatingequipmenttype = 'VSC_CONVERTER_STATION'
+              and regulatingpoint.regulatingequipmentid = vscconverterstation.id
+              and regulatingpoint.networkuuid = vscconverterstation.networkuuid
+              and regulatingpoint.variantnum = vscconverterstation.variantnum)
+where regulatingequipmenttype = 'VSC_CONVERTER_STATION';
 -- shunt
 update regulatingpoint
-set regulating = shuntcompensator.voltageregulatoron
-from shuntcompensator
-where regulatingpoint.regulatingequipmenttype = 'SHUNT_COMPENSATOR'
-  and regulatingpoint.regulatingequipmentid = shuntcompensator.id
-  and regulatingpoint.networkuuid = shuntcompensator.networkuuid
-  and regulatingpoint.variantnum = shuntcompensator.variantnum;
-
+set regulating = (select shuntcompensator.voltageregulatoron from shuntcompensator
+            where regulatingpoint.regulatingequipmenttype = 'SHUNT_COMPENSATOR'
+              and regulatingpoint.regulatingequipmentid = shuntcompensator.id
+              and regulatingpoint.networkuuid = shuntcompensator.networkuuid
+              and regulatingpoint.variantnum = shuntcompensator.variantnum)
+where regulatingequipmenttype = 'SHUNT_COMPENSATOR';
 -- svc
 update regulatingpoint
 set regulating = (CASE WHEN regulationmode = 'VOLTAGE'
