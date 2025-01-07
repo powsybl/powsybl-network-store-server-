d7ef98c525c1403b031c9c89c5eecec6650175da
Etienne Homer <etiennehomer@gmail.com>
2025-01-07 11:28:38 +0100

    move MigrationController + move sonar exclusion property
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- network-store-server/src/main/java/com/powsybl/network/store/server/migration/MigrationController.java
+++ network-store-server/src/main/java/com/powsybl/network/store/server/migration/MigrationController.java
@@ -4,9 +4,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package com.powsybl.network.store.server;
+package com.powsybl.network.store.server.migration;
 
 import com.powsybl.network.store.model.*;
+import com.powsybl.network.store.server.NetworkStoreRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
